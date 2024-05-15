2768f05f65b49aa663851d88a0783aad84bec4b7
Finbar Dennehy <finny.d@gmail.com>
2024-05-15 22:16:19 +0100

    correlation added to Jupter Notebook

--- analysis.py
+++ analysis.py
@@ -4,9 +4,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Create a pandas DataFrame based on the iris.csv file
-df = pd.read_csv('iris.csv')
+# Create a pandas DataFrame from a csv (providing url of the raw data)
+df = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+
+# Write the DataFrame as a csv so we have a local copy
+df.to_csv('iris.csv')
+
+# https://note.nkmk.me/en/python-pandas-agg-aggregate/
+print(df.select_dtypes(include='number').agg(['mean', 'median', 'mode', 'kurtosis']))
 
 # Describe the data set and print the output to 'variable_summary.csv'
-df.describe().to_csv('variable_summary.csv')
+# https://practicaldatascience.co.uk/data-science/how-to-create-descriptive-statistics-using-the-pandas-describe-function
+df.describe(include='all').T.to_csv('variable_summary.csv')
+
+
+# https://www.geeksforgeeks.org/loop-or-iterate-over-all-or-certain-columns-of-a-dataframe-in-python-pandas/
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.items.html
 
